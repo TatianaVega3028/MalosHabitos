9105ff88dc8d59313c32d3f6b1083d6737b485b9
Tati <75193605@continental.edu.pe>
2024-09-09 14:53:45 -0500

    Modificación ejercicio 01

---
+++ Ejemploo1.py
@@ -0,0 +1,12 @@
+def multiplicacion(factor1, factor2):
+    producto = factor1 * factor2
+    return producto
+
+
+if __name__ == "__main__":
+    multiplicando = float(input("multiplicando: "))
+    multiplicador = float(input("multiplicador: "))
+
+    resultado = multiplicacion(multiplicando, multiplicador)
+
+    print(f"{multiplicando} * {multiplicador} = {resultado}")
